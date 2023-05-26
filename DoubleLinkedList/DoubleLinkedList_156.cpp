ff98ac33badb3477429b8d05c51dbd8ee6745e3e
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-26 14:30:31 +0700

    13

--- DoubleLinkedList/DoubleLinkedList_156.cpp
+++ DoubleLinkedList/DoubleLinkedList_156.cpp
@@ -117,3 +117,14 @@ void DoubleLinkedList::revtraverse() {
 	if (listEmpety()) {
 		cout << "\nList is empety" << endl;
 	}
+	else {
+		cout << "\nRecords in descending order of roll number are :" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
