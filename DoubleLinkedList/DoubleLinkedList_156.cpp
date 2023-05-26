223957a5f6c38e05c9465646cd8b6fde8c5bba49
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-26 14:04:50 +0700

    previous

--- DoubleLinkedList/DoubleLinkedList_156.cpp
+++ DoubleLinkedList/DoubleLinkedList_156.cpp
@@ -33,22 +33,37 @@ void DoubleLinkedList::addNode() {
 	int nim;
 	string nm;
 	cout << "\nEnter the roll number of the student:";
-		cin >> nim;
+	cin >> nim;
 	cout << "\nEnter the name of the student:";
-		cin >> nm;
-		Node* newNode = new Node();
-		newNode->noMhs = nim;
-		newNode->name = nm;
+	cin >> nm;
+	Node* newNode = new Node();
+	newNode->noMhs = nim;
+	newNode->name = nm;
 
 
-		if (START == NULL || nim <= START->noMhs) {
-			if (START != NULL && nim == START->noMhs)
-				cout << "\nDuplicate number not allowed" << endl;
-			return;
-		}
-		newNode->next = START;
-		if (START != NULL)
-			START->prev = newNode;
-		newNode->prev = NULL;
-		START = newNode;
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+			cout << "\nDuplicate number not allowed" << endl;
 		return;
+	}
+	newNode->next = START;
+	if (START != NULL)
+		START->prev = newNode;
+	newNode->prev = NULL;
+	START = newNode;
+	return;
+}
+/*inserting a Node Between Two Nodes in the list*/
+Node* current = START; // step 1.a
+Node* previous = NULL; // step 1.b
+while (current->next != NULL && current->next->noMhs < nim) // step 1.c
+{
+	{
+		previous = current; // step 1.d
+		current = current->next; // step 1.e
+	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
