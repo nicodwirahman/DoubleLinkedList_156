1c0a7821b04030d4425bbf7c023a67bc0a09208b
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-26 12:58:43 +0700

    class DoubleLinkedList

--- DoubleLinkedList/DoubleLinkedList_156.cpp
+++ DoubleLinkedList/DoubleLinkedList_156.cpp
@@ -9,3 +9,17 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool searach(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void revtraverse();
+	void hapus();
+	void searchData();
+};
